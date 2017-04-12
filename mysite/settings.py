1dbc1ee786818b5e96e6da0d419285cedbb08535
Erik Zorn <erik.zorn44@gmail.com>
2017-04-11 23:19:29 -0400

    mhm

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,9 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-#TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-#TEMPLATE_DIRS = ['/Users/erikzorn/project/mysite/templates']
+#TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), 'templates')]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
